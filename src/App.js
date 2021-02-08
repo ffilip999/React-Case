d2b057dec7f64ce597c7c486d564237a79f903e6
unknown <filip.norstrom@student.kyh.se>
2021-02-08 20:28:44 +0100

    väder api klart

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
+import Weather from './components/Weather';
 
 
 function App() {
@@ -13,9 +15,11 @@ function App() {
         <Navbar />
          <switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Weather}/>
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </switch>
+        <Footer />
       </div>
     </Router>
   );
@@ -23,7 +27,8 @@ function App() {
 
 const Home = () => (
   <div>  
-    <h1 className='head'>Hemma</h1>
+    <h1 className='head'>Weather</h1>
+    <p className="head-info">Search for your current weather in your city or country</p>
   </div>
 )
 
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
