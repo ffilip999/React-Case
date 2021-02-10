d45b3c9edf305e608fa86f70044fa63d9dbd7422
unknown <filip.norstrom@student.kyh.se>
2021-02-10 16:52:12 +0100

    small fix

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,18 @@ import './About.css';
 
 function About() {
     return (
-        <div>
-            <h1 className='head'>About me</h1>
+        
+        <div className="about-wrapper">
+            <div className="about-me">
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis purus nunc, mollis ut tempus in, imperdiet at leo. Donec rhoncus lacus quis fermentum porttitor. Nulla nec magna faucibus, varius magna at, auctor odio. Fusce gravida ultrices ante et finibus. Nunc tincidunt tellus quis leo suscipit, sit amet dictum nulla sodales. Nullam bibendum metus velit, in sagittis dui finibus a. In vulputate, nulla eget gravida posuere, tellus lorem vestibulum metus, at dignissim augue diam eget lectus. Vestibulum vehicula nunc ac ultricies porttitor. Sed dignissim quis dui sed egestas. Praesent facilisis eleifend laoreet. Aliquam quis dignissim magna. Sed volutpat metus non dapibus viverra. Sed eu imperdiet lorem, in placerat ex.</li>
+                    <br></br>
+                    <li>Maecenas nibh arcu, venenatis nec varius at, tincidunt fringilla dui. Integer vitae nisi a nisl interdum faucibus aliquam sed ipsum. Pellentesque mollis diam condimentum, varius eros vehicula, dapibus nibh. Fusce sed egestas lacus. Aliquam sed nisi quam. Proin ultricies rutrum lorem, nec aliquet ex gravida quis. Duis purus lectus, aliquet eu volutpat in, luctus eu sapien. In malesuada, velit at sodales vulputate, lorem metus condimentum nulla, tempor finibus mauris libero sit amet ex. Ut pretium odio non nisl tristique, id rutrum mauris commodo. Sed faucibus ipsum sit amet dolor tristique maximus. Fusce mattis sagittis felis, et blandit lorem rutrum nec. Donec imperdiet maximus dolor, vitae finibus ex sagittis eu. Curabitur vehicula lorem eget purus congue, at aliquet dolor pulvinar. Morbi id tempor augue, ut bibendum enim. Phasellus id porttitor lacus, ac suscipit justo.</li>
+                </ul>
+            </div>
+            <div className="img-container">
+                <img className="img-me" src="https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"></img>
+            </div>
         </div>
     );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,9 @@ function Contact() {
         <div className='wrapper'>
             <div className='contact-div'>
                 <h1 className='head'>Contact information</h1>
-                <p className='contact'>Email addres:</p> <a className='contact' href='#'>FIlip.norstrom@hotmail.se</a>
-                <p className='contact'>Linkedin:</p><a className='contact' href='#'>Filip Norström</a>
+                <p className='email'>Email addres:</p> <p className='contact' href='#'>FIlip.norstrom@hotmail.se</p>
+                <a className='contact' href='https://www.linkedin.com/in/filip-norstr%C3%B6m-a3228a204/'>Linkedin</a>
+                <a className='contact' href='https://github.com/ffilip999'>Github</a>
             </div>
             <img className='img-dec' src='https://images.pexels.com/photos/33153/raisting-sattelit-reception-signal.jpg'></img>
         </div>
